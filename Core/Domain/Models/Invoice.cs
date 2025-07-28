3342d67da97ba45bbbdc481961ec18dc291c5ba8
Admin <Admin@DESKTOP-1R0450K>
2025-07-28 20:08:51 +0400

    Module and DbContext

---
+++ Core/Domain/Models/Invoice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Invoice : BaseEntity<int>
+    {
+        //  public int InvoiceId { get; set; }
+        public int OrderId { get; set; } 
+        public Order  order { get; set; }  // One
+        public DateTime InvoiceDate { get; set; } = DateTime.UtcNow;
+        public decimal TotalAmount { get; set; }
+
+
+
+    }
+}
